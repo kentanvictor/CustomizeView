853a3d1b38123c197ba4c93978b4356e68e8aefb
kentanvictor <2030457926@qq.com>
2018-02-28 11:20:03 +0800

    third commit

--- app/src/main/java/com/example/dell/customizeview/MainActivity.java
+++ app/src/main/java/com/example/dell/customizeview/MainActivity.java
@@ -4,27 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-//                            _ooOoo_
-//                           o8888888o
-//                           88" . "88
-//                           (| -_- |)
-//                            O\ = /O
-//                        ____/`---'\____
-//                      .   ' \\| |// `.
-//                       / \\||| : |||// \
-//                     / _||||| -:- |||||- \
-//                       | | \\\ - /// | |
-//                     | \_| ''\---/'' | |
-//                      \ .-\__ `-` ___/-. /
-//                   ___`. .' /--.--\ `. . __
-//                ."" '< `.___\_<|>_/___.' >'"".
-//               | | : `- \`.;`\ _ /`;.`/ - ` : | |
-//                 \ \ `-. \_ __\ /__ _/ .-` / /
-//         ======`-.____`-.___\_____/___.-`____.-'======
-//                            `=---='
-//
-//         .............................................
-//                  佛祖保佑             永无BUG
 
 public class MainActivity extends AppCompatActivity {
     private CirclePercentView mCirclePercentView;
