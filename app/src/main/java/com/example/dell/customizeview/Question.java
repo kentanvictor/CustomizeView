bfa7414b4ffba58a8e9fea5d375b4c264b5f3c54
kentanvictor <2030457926@qq.com>
2018-03-04 09:16:04 +0800

    forth commit

--- app/src/main/java/com/example/dell/customizeview/Question.java
+++ app/src/main/java/com/example/dell/customizeview/Question.java
@@ -1,10 +1,27 @@
 package com.example.dell.customizeview;
 
-/**
+/*
  * Created by 20304 on 2018/2/11.
  */
 
 public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
 }
